c675c25d3be9d1d3dc0bf2b44337ac539c5afb62
Yefim Shneyderman <yshneyderman@umass.edu>
2021-03-27 02:46:28 -0400

    Dashboard Basics

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# Salary-Visualizer
+## Salary-Visualizer
 Visualize personal salary over course of a work session to motivate, track, and reflect on working time
+The tool is made using HTML, CSS, and vanilla JavaScript so as to be run locally by any user with minimal technical experience.
+All calculations and storage is done locally in browser so no information is sent anywhere.
+
+# Instructions for use:
+* Clone the repository
+* Open index.html in your browser
+* Add your salary and other info and run the tool
+
+Feel free to host this tool on the web and allow others to use it as well.
--- script.js
+++ script.js
@@ -1,74 +1,165 @@
-let startABS = new Date().getTime();
-let seconds = 0;
-let money = 0.00;
-let salary = 32.00;
-let pause = false;
-let view = false;
-let fps = 24;
-window.onload = startTime;
-
 function startTime() {
-    salary = document.getElementById("salary").elements[0].value;
-    if(pause == false){
-        //check if session already had started
-        if (typeof(Storage) !== "undefined" && seconds == 0) {
-            money = parseFloat(localStorage.getItem("money"));
-        }
-        var today = new Date();
-        seconds = parseInt((today.getTime() - startABS)/1000, 10);
-        document.getElementById('money').innerHTML = "$" + calculateMoney(seconds);
-        document.getElementById('dollar').innerHTML = '</div><div style="background-color: #e6e6e6; width: 200px; border-color:#333333; border-style: solid; border-width: 1px; height: 25px; margin-top: -1px; margin-left: 99px; position: absolute;"></div><div style="background-color: green; width: ' + (200*(money%1))%200 + 'px; height: 25px; margin-left: 100px; position: absolute;">'
-        //save every 5 seconds
-        if(seconds%5 == 0){
-            localStorage.setItem("money", money);
-        }
-        var t = setTimeout(startTime, 1000/fps);
-    }
+    //check if session already had started
+    
 }
 
-function toggleStop(){
-    pause = !pause;
-    if(pause == false){
-        var t = setTimeout(startTime, 1000);
-        document.getElementById('stop').innerHTML = '<i class="fas fa-pause-circle"></i>&nbsp;&nbsp;Pause';
+let days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul","Aug", "Sep", "Oct", "Nov", "Dec"];
+
+function runCode() {
+    let curr = new Date();
+    let time = "Current Time/Date: " + curr.getHours() + ":" + curr.getMinutes() + " " + days[curr.getDay()-1] + " " + months[curr.getMonth()] + " " + curr.getDate() + " " + curr.getFullYear();
+    document.getElementById("datetime").innerHTML = time;
+    var t = setTimeout(function(){ runCode() }, 1000); 
+    //Check that storage is not undefined
+    if (typeof(Storage) !== "undefined") {
+        salaryCreated = parseInt(localStorage.getItem("salaryCreated"));
+        workStarted = parseInt(localStorage.getItem("workStarted"));
+        accountCreated = parseInt(localStorage.getItem("accountCreated"));
+
+        if(accountCreated !== 1){
+            localStorage.setItem("totalPay", 0.00);
+            localStorage.setItem("totalHours", 0.00);
+            localStorage.setItem("totalTakeHome", 0.00);
+            localStorage.setItem("accountCreated" , 1);
+            localStorage.setItem("sessions" , '{"sessions":[]}');
+        }
+
+        if(accountCreated === 1){
+            let contents = "\
+            <div style='float: left; width: 35%'>\
+                <p class='subtext'>Account</p><br>\
+                <p class='midtext'>Position: " + localStorage.getItem("position") + "</p>\
+                <p class='midtext' style='float:left'>Hourly Wage: <p class='midtext' style='color: var(--green); float: left; margin-left: 4px'>$" + (parseFloat(localStorage.getItem("salary"))).toFixed(2) + "/hr</p></p>\
+                <p class='midtext' style='float:left'>Applied Tax: <p class='midtext' style='color: var(--green); float: left; margin-left: 4px'> " + (localStorage.getItem("tax")*100).toFixed(1) + "%</p></p>\
+            </div>\
+            <div style='float: left; width: 28%'>\
+                <p class='subtext'>Value</p>\
+                <p class='subtext'>Investing value of total funds</p><br>\
+                <p class='midtext' style='float:left'>S&P500 1 Year: <p class='midtext' style='color: var(--green); float: left; margin-left: 4px'>$" + (parseFloat(localStorage.getItem("totalTakeHome"))*Math.pow((1+0.10/1.0), 1)).toFixed(2) + "</p></p>\
+                <p class='midtext' style='float:left'>S&P500 10 Year: <p class='midtext' style='color: var(--green);margin-left: 4px'>$" + (parseFloat(localStorage.getItem("totalTakeHome"))*Math.pow((1+0.10/1.0), 10)).toFixed(2) + "</p>\
+                <p class='midtext' style='float:left'>S&P500 30 Year: <p class='midtext' style='color: var(--green); float: left; margin-left: 4px'>$" + (parseFloat(localStorage.getItem("totalTakeHome"))*Math.pow((1+0.10/1.0), 30)).toFixed(2) + "</p></p>\
+            </div>\
+            <div style='float: left; width: 30%'>\
+                <p class='subtext'>Expenses</p><br>\
+                <p class='midtext' style='float:left'>Hours Worked: <p class='midtext' style='color: var(--green); float: left; margin-left: 4px'>" + (parseFloat(localStorage.getItem("hours"))).toFixed(2) + " hrs</p></p>\
+                <p class='midtext' style='float:left; margin-right: 4px;'>Gross Pay: <p class='midtext' style='color: var(--green); margin-left: 4px'> $" + (parseFloat(localStorage.getItem("pay"))).toFixed(2) + "</p>\
+                <p class='midtext' style='float:left'>Take Home Pay: <p class='midtext' style='color: var(--green); float: left; margin-left: 4px'> $" + (parseFloat(localStorage.getItem("takeHome"))).toFixed(2) + "</p></p>\
+            </div>\
+            <i class='fa fa-times-circle' aria-hidden='true' id='icon-default' style='position: absolute; right:3%; margin-top: 0px; cursor: pointer' onclick='deleteSalary()'></i>"
+            document.getElementById("account-inside").innerHTML = contents;
+        }
+
+        if(salaryCreated === NaN){
+            localStorage.setItem("salaryCreated", 0);
+            salaryCreated = 0;
+        }
+        if(salaryCreated === 1){
+            let contents = "\
+            <div style='float: left; width: 27%'>\
+                <p class='subtext'>Salary</p><br>\
+                <p class='midtext'>Position: " + localStorage.getItem("position") + "</p>\
+                <p class='midtext' style='float:left'>Hourly Wage: <p class='midtext' style='color: var(--green); float: left; margin-left: 4px'>$" + (parseFloat(localStorage.getItem("salary"))).toFixed(2) + "/hr</p></p>\
+                <p class='midtext' style='float:left'>Applied Tax: <p class='midtext' style='color: var(--green); float: left; margin-left: 4px'> " + (localStorage.getItem("tax")*100).toFixed(1) + "%</p></p>\
+            </div>\
+            <div style='float: left; width: 18%'>\
+                <p class='subtext'>Controls</p><br>\
+                <div style='float: left; text-align: center; margin-right: 10px'>\
+            "
+            if(workStarted === 1){
+                curr = new Date();
+                localStorage.setItem("pay", (curr.getTime() - parseInt(localStorage.getItem("timeStarted")))/(60*60*1000)*parseFloat(localStorage.getItem("salary")));
+                localStorage.setItem("hours", (curr.getTime() - parseInt(localStorage.getItem("timeStarted")))/(60*60*1000));
+                localStorage.setItem("takeHome", (curr.getTime() - parseInt(localStorage.getItem("timeStarted")))/(60*60*1000)*parseFloat(localStorage.getItem("salary")*parseFloat(localStorage.getItem("tax"))));
+                contents += "<i id='icon-default' class='fa fa-pause-circle' aria-hidden='true'; style='font-size: 1.7em; margin-bottom: 3px; cursor: pointer' onclick='stopWork()'></i>\
+                <p class='subtext'>Stop Work</p>";
+            }
+            else{
+                contents += "<i id='icon-default' class='fa fa-play-circle' aria-hidden='true'; style='font-size: 1.7em; margin-bottom: 3px; cursor: pointer' onclick='startWork()'></i>\
+                <p class='subtext'>Start Work</p>";
+                
+            }
+            contents += "\
+                </div>\
+                <div style='float: left; text-align: center'>\
+                    <i id='icon-default' class='fa fa-download' aria-hidden='true' style='font-size: 1.7em; margin-bottom: 3px;'></i>\
+                    <p class='subtext'>Download .CSV</p>\
+                </div>\
+            </div>\
+            <div style='float: left; width: 23%'>\
+                <p class='subtext'>Session Pay</p><br>\
+                <p class='midtext' style='float:left'>Hours Worked: <p class='midtext' style='color: var(--green); float: left; margin-left: 4px'>" + (parseFloat(localStorage.getItem("hours"))).toFixed(2) + " hrs</p></p>\
+                <p class='midtext' style='float:left; margin-right: 4px;'>Gross Pay: <p class='midtext' style='color: var(--green); margin-left: 4px'> $" + (parseFloat(localStorage.getItem("pay"))).toFixed(2) + "</p>\
+                <p class='midtext' style='float:left'>Take Home Pay: <p class='midtext' style='color: var(--green); float: left; margin-left: 4px'> $" + (parseFloat(localStorage.getItem("takeHome"))).toFixed(2) + "</p></p>\
+            </div>\
+            <div style='float: left; width: 30%'>\
+                <p class='subtext'>Sessions</p>\
+            </div>\
+            <i class='fa fa-times-circle' aria-hidden='true' id='icon-default' style='position: absolute; right:3%; margin-top: 0px; cursor: pointer' onclick='deleteSalary()'></i>\
+            <div style='position: absolute; z-index: 1; float: left; left: 40.5%; width: 150px; background-color:var(--green); height: 5px; margin-top:-13px; margin-bottom:-18px;'>&nbsp;<div>\
+            <div style='position: absolute; z-index: 2; float: left; width: 150px; background-color:var(--green); bottom: -102px; height: 5px; margin-top:-13px; margin-bottom:-18px;'>&nbsp;<div>"
+
+            document.getElementById("salary-inside").innerHTML = contents;
+        }
+        else if(salaryCreated === 0){
+            document.getElementById("salary-inside").innerHTML = "\
+            <p class='subtext'>Salary</p>\
+            <div style='text-align:center'>\
+                <i class='fa fa-plus-circle' aria-hidden='true' id='icon-default' style='font-size: 2em; margin-top: 10px; margin-bottom: 5px; cursor: pointer' onclick='openSalaryModal()'></i>\
+                <p class='subtext'>Create salary</p>\
+            </div>"
+        }
     }
     else{
-        document.getElementById('stop').innerHTML = '<i class="fas fa-play-circle"></i>&nbsp;&nbsp;Resume';
+        localStorage.setItem("salaryCreated", 0);
     }
+  }
+
+  function startWork(){
+    localStorage.setItem("pay", 0);
+    localStorage.setItem("takeHome", 0);
+    localStorage.setItem("hours", 0.00);
+    localStorage.setItem("workStarted", 1);
+    let curr = new Date();
+    localStorage.setItem("timeStarted", curr.getTime());
+  }
+
+  function stopWork(){
+    localStorage.setItem("totalPay", parseFloat(localStorage.getItem("totalPay")) + parseFloat(localStorage.getItem("pay")));
+    localStorage.setItem("totalTakeHome", parseFloat(localStorage.getItem("totalTakeHome")) + parseFloat(localStorage.getItem("takeHome")));
+    localStorage.setItem("totalHours", parseFloat(localStorage.getItem("totalHours")) + parseFloat(localStorage.getItem("hours")));
+    localStorage.setItem("workStarted", 0);
+    let obj = JSON.parse(localStorage.getItem("sessions"));
+    obj.sessions.push('{' + '"Date":' + new Date(parseFloat(localStorage.getItem("timeStarted"))).toDateString() + ',' + '"Take Home Pay":' + parseFloat(localStorage.getItem("takeHome")).toFixed(2) + '"Gross Pay":' + parseFloat(localStorage.getItem("pay")).toFixed(2)+ '}');
+    localStorage.setItem("sessions", JSON.stringify(obj));
+    let curr = new Date();
+    localStorage.setItem("timeEnded", curr.getTime());
 }
 
-function changeSalary(){
-    salary = document.getElementById("salary").elements[0].value;
-}
+  function openSalaryModal(){
+    document.getElementById("salaryModal").style.display="block";
+  }
 
-function toggleView(){
-    view = !view;
-    if(view == false){
-        document.getElementById('view').innerHTML = '<i class="fas fa-angle-double-down"></i>&nbsp;&nbsp;View More';
-        document.getElementById("left").innerHTML = '';
-        document.getElementById("right").innerHTML = '';
-    }
-    else{
-        document.getElementById('view').innerHTML = '<i class="fas fa-angle-double-up"></i>&nbsp;&nbsp;View Less';
-        document.getElementById("left").innerHTML = '<p>Yo</p>';
-        document.getElementById("right").innerHTML = '<p>Yo</p>';
-    }
-}
-
-function checkTime(i) {
-    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
-    return i;
-}
-
-function calculateMoney(seconds){
-    money += parseFloat((salary/3600)/fps);
-    return money.toFixed(2);
-}
-
-function restart(){
-    console.log("RESTART");
-    money = 0.00;
-    localStorage.setItem("money", money);
-}
+  function createSalary(){
+    let inputs = document.getElementById("salaryForm").elements;
+    console.log(inputs);
+    let position = inputs[0].value;
+    let salary = inputs[1].value;
+    let tax = inputs[2].value;
+    localStorage.setItem("position", position);
+    localStorage.setItem("salary", salary);
+    localStorage.setItem("tax", tax);
+    localStorage.setItem("hours", 0);
+    localStorage.setItem("pay", 0);
+    localStorage.setItem("takeHome", 0);
+    localStorage.setItem("workStarted", 0);
+    document.getElementById("salaryModal").style.display="none";
+    localStorage.setItem("salaryCreated", 1);
+  }
 
 
+  function deleteSalary(){
+    localStorage.setItem("salaryCreated", 0);
+  }
+
+  runCode();
